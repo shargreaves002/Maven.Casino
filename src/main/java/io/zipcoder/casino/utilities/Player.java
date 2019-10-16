6fa92b0d985d79e605335076f9e9f0098746d298
shishonah <ms.momo247@gmail.com>
2019-10-16 18:11:35 -0400

    did blackjack I think

--- src/main/java/io/zipcoder/casino/utilities/Player.java
+++ src/main/java/io/zipcoder/casino/utilities/Player.java
@@ -1,4 +1,42 @@
 package io.zipcoder.casino.utilities;
 
+import java.util.ArrayList;
+
 public class Player implements GamblingPlayer {
+    private ArrayList<Card> hand = new ArrayList<Card>();
+    private String name;
+    private boolean isPlaying = true;
+    private int score = 0;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    ArrayList<Card> getHand(){
+        return hand;
+    }
+
+    void addToHand(Card card){
+        hand.add(card);
+    }
+
+    String getName(){
+        return name;
+    }
+
+    void setPlaying(boolean isPlaying){
+        this.isPlaying = isPlaying;
+    }
+
+    boolean isPlaying(){
+        return isPlaying;
+    }
+
+    int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
