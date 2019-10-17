26fba2bb46dc5854a5978dc391cf2b31012a76d3
Sarah <sarahhargreaves10@gmail.com>
2019-10-16 22:48:56 -0400

    Built Craps

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -1,8 +1,12 @@
 package io.zipcoder.casino;
-
+import io.zipcoder.casino.utilities.*;
 
 public class Casino {
     public static void main(String[] args) {
-        // write your tests before you start fucking with this
+        Player sarah = new Player("Sarah");
+        Player carlos = new Player("Carlos");
+        Player malcolm = new Player("Malcolm");
+        Craps craps = new Craps();
+        craps.play(sarah, carlos, malcolm);
     }
 }
